c89897ffa5a1f8ac7bb2bf64b26762845a6c8f5c
gwagjiug <kwjo0228@naver.com>
2024-08-23 11:49:44 +0900

    useRef

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,7 +7,8 @@
 //사용자가 텍스트를 입력하면 입력된 텍스트를 name state에 저장
 //최종적으로 name state에 입력한 값이 보관이 될 것임
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+
 const Register = () => {
   const [input, setInput] = useState({
     name: '',
@@ -16,8 +17,14 @@ const Register = () => {
     bio: '',
   });
 
+  const countRef = useRef(0);
+  //useRef는 useState와 같이 값을 저장하는 변수로 사용되지만 state처럼 값이 변경될 때마다 리랜더링이 발생하지는 않는다.
+  //js 변수를 사용할 수도 있으나, js 변수는 리랜더링 되면 계속초기화 되고, useRef를 사용하면 리랜더링이 되더라도 값을 계속 저장하기 때문임
+  const inputRef = useRef();
+
   const onChange = (e) => {
-    console.log(e.target.name, e.target.value);
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
@@ -25,10 +32,17 @@ const Register = () => {
     });
   };
 
+  const onSubmit = (e) => {
+    if (input.name === '') {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -56,6 +70,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange}></textarea>
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
