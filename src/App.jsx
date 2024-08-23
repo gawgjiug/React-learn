70a9b86fbb50024a4a206d6ae73da0bbe2716124
gwagjiug <kwjo0228@naver.com>
2024-08-23 15:35:04 +0900

    Hooks & custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { useState } from 'react';
 import Register from './components/Register';
+import HookExam from './components/HookExam';
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <Register />
+      <HookExam />
     </>
   );
 }
