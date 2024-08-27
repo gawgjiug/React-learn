292b8f1cb1e15e037cd4cea692c42fd4a0e69512
gwagjiug <kwjo0228@naver.com>
2024-08-27 10:33:13 +0900

    file 구조 변경 및 Todo 추가

---
+++ Todo/src/components/List.jsx
@@ -0,0 +1,45 @@
+import './List.css';
+import TodoItem from './TodoItem';
+import { useState } from 'react';
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState('');
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredDate = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filterdTodos = getFilteredDate();
+  return (
+    <div className="List">
+      <h4>Todo List🧩</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요🔍"
+      />
+      <div className="todos_wrapper">
+        {filterdTodos.map((todo) => {
+          return (
+            <TodoItem
+              {...todo}
+              key={todo.id}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
